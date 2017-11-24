ff3a86baee9fdf72212703dcdd4fe18423e69ef8
cherryBomba58 <repassy.adrienn@gmail.com>
2017-11-24 15:10:36 +0100

    Course CRUD functions

--- LanguageLearner/LanguageLearner.ANG/Controllers/CourseController.cs
+++ LanguageLearner/LanguageLearner.ANG/Controllers/CourseController.cs
@@ -28,27 +28,40 @@ namespace LanguageLearner.WebAPI.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            CourseModel course = _manager.GetCourseById(id);
+            return Json(course);
         }
 
         // POST api/values
+        // A CourseModel nem tartalmazza  Learnable-k listáját,
+        // a Learnable-ket viszont érdemes másik kontrollerben (LearnableController),
+        // másik Post-ban küldeni az adatbázis felé, akár Learnable-k tömbjét is
+        // (az is lehet külön LearnableListModel, amely csak  Learnable-k listáját tartalmazza),
+        // és azoknak megadni a kurzusuk kulcsát, az ő külső kulcsukként.
+        // A CourseModel id-jára sincs most szükség, csak szemléltetés, hogy modelleket kell itt is alkalmazni.
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]CourseModel model)
         {
+            _manager.AddCourse(model);
+            return Json("OK");
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]CourseModel model)
         {
+            _manager.PutCourse(id, model);
+            return Json("OK");
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            _manager.DeleteCourse(id);
+            return Json("OK");
         }
     }
 }
--- LanguageLearner/LanguageLearner.BLL/Managers/CourseManager.cs
+++ LanguageLearner/LanguageLearner.BLL/Managers/CourseManager.cs
@@ -1,5 +1,6 @@
 ﻿using LanguageLearner.BLL.Models;
 using LanguageLearner.DAL.Context;
+using LanguageLearner.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,43 @@ namespace LanguageLearner.BLL.Managers
                 CourseID = c.CourseID,
                 Name = c.Name
             }).ToList();
+        }
+
+        public CourseModel GetCourseById(int id)
+        {
+            var course = _context.Courses.Single(c => c.CourseID == id);
+            return new CourseModel
+            {
+                CourseID = course.CourseID,
+                Name = course.Name
+            };
+        }
+
+        public void AddCourse(CourseModel model)
+        {
+            // ID-t majd kap, ha bevettük az adatbázisba
+            var course = new Course
+            {
+                Name = model.Name
+            };
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+        }
+
+        public void PutCourse(int id, CourseModel model)
+        {
+            // itt már egy megadott ID alapján keresünk egy kurzust,
+            // és azt módosítjuk
+            var course = _context.Courses.Single(c => c.CourseID == id);
+            course.Name = model.Name;
+            _context.SaveChanges();
+        }
+
+        public void DeleteCourse(int id)
+        {
+            var course = _context.Courses.Single(c => c.CourseID == id);
+            _context.Courses.Remove(course);
+            _context.SaveChanges();
         }
     }
 }
