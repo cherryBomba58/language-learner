580c7a4588bbd5e28f4193fead5fff749431331e
Annosz <tadamleanyfalu@gmail.com>
2017-11-25 18:32:41 +0100

    Fully editable words.

--- LanguageLearner/LanguageLearner.BLL/Managers/LearnableManager.cs
+++ LanguageLearner/LanguageLearner.BLL/Managers/LearnableManager.cs
@@ -1,5 +1,6 @@
 ﻿using LanguageLearner.BLL.Models;
 using LanguageLearner.DAL.Context;
+using LanguageLearner.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -19,7 +20,20 @@ namespace LanguageLearner.BLL.Managers
             _context = context;
         }
 
-        public IEnumerable<LearnableModel> GetLearnables(int courseID)
+        public IEnumerable<LearnableModel> GetLearnables()
+        {
+            return _context.Learnables.Select(l => new LearnableModel
+            {
+                LearnableID = l.LearnableID,
+                CourseID = l.CourseID,
+                English = l.English,
+                Hungarian = l.Hungarian,
+                PictureUrl = l.PictureUrl,
+                WordClass = l.WordClass
+            }).ToList();
+        }
+
+        public IEnumerable<LearnableModel> GetLearnablesByCourseId(int courseID)
         {
             return _context.Courses
                 .Include(c => c.LearnableList)
@@ -27,13 +41,48 @@ namespace LanguageLearner.BLL.Managers
                 .LearnableList
                 .Select(l => new LearnableModel
                 {
+                    LearnableID = l.LearnableID,
                     CourseID = l.CourseID,
                     English = l.English,
                     Hungarian = l.Hungarian,
-                    LearnableID = l.LearnableID,
                     PictureUrl = l.PictureUrl,
                     WordClass = l.WordClass
                 }).ToList();
+        }
+
+        public void AddLearnable(LearnableModel model)
+        {
+            // It will get ID automatically in the DB
+            var learnable = new Learnable
+            {
+                CourseID = model.CourseID,
+                English = model.English,
+                Hungarian = model.Hungarian,
+                PictureUrl = model.PictureUrl,
+                WordClass = model.WordClass
+            };
+            _context.Learnables.Add(learnable);
+            _context.SaveChanges();
+        }
+
+        public void PutLearnable(int id, LearnableModel model)
+        {
+            // Search by the given ID the modify
+            var learnable = _context.Learnables.Single(l => l.LearnableID == id);
+            learnable.CourseID = model.CourseID;
+            learnable.English = model.English;
+            learnable.Hungarian = model.Hungarian;
+            learnable.PictureUrl = model.PictureUrl;
+            learnable.WordClass = model.WordClass;
+            _context.SaveChanges();
+        }
+
+        public void DeleteLearnable(int id)
+        {
+            // Search by the given ID then delete
+            var learnable = _context.Learnables.Single(l => l.LearnableID == id);
+            _context.Learnables.Remove(learnable);
+            _context.SaveChanges();
         }
     }
 }
--- LanguageLearner/LanguageLearner.WPF/Menu/CourseOverview.xaml.cs
+++ LanguageLearner/LanguageLearner.WPF/Menu/CourseOverview.xaml.cs
@@ -29,7 +29,7 @@ namespace LanguageLearner.WPF.Menu
             InitializeComponent();
             this.DataContext = this;
 
-            InitCourses();
+            InitCoursesList();
         }
 
         public void UtilizeState(object state)
@@ -40,15 +40,14 @@ namespace LanguageLearner.WPF.Menu
 
 
         #region Properties
-        public String NewCourseName
+        public CourseModel NewCourse
         {
-            get { return (String)GetValue(NewCourseNameProperty); }
-            set { SetValue(NewCourseNameProperty, value); }
+            get { return (CourseModel)GetValue(NewCourseProperty); }
+            set { SetValue(NewCourseProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for NewCourseName.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty NewCourseNameProperty =
-            DependencyProperty.Register("NewCourseName", typeof(String), typeof(CourseOverview), new PropertyMetadata(""));
+        
+        public static readonly DependencyProperty NewCourseProperty =
+            DependencyProperty.Register("NewCourse", typeof(CourseModel), typeof(CourseOverview), new PropertyMetadata(new CourseModel()));
 
 
 
@@ -57,8 +56,7 @@ namespace LanguageLearner.WPF.Menu
             get { return (List<CourseModel>)GetValue(CourseListProperty); }
             set { SetValue(CourseListProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        
         public static readonly DependencyProperty CourseListProperty =
             DependencyProperty.Register("CourseList", typeof(List<CourseModel>), typeof(CourseOverview), new PropertyMetadata(new List<CourseModel>()));
 
@@ -69,40 +67,84 @@ namespace LanguageLearner.WPF.Menu
             get { return (CourseModel)GetValue(SelectedCourseProperty); }
             set { SetValue(SelectedCourseProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for selectedCourse.  This enables animation, styling, binding, etc...
+        
         public static readonly DependencyProperty SelectedCourseProperty =
             DependencyProperty.Register("SelectedCourse", typeof(CourseModel), typeof(CourseOverview), new PropertyMetadata(null));
 
 
+
+        public List<LearnableModel> Learnables
+        {
+            get { return (List<LearnableModel>)GetValue(LearnablesProperty); }
+            set { SetValue(LearnablesProperty, value); }
+        }
+        
+        public static readonly DependencyProperty LearnablesProperty =
+            DependencyProperty.Register("Learnables", typeof(List<LearnableModel>), typeof(CourseOverview), new PropertyMetadata(new List<LearnableModel>()));
+
+
+
+        public LearnableModel SelectedLearnable
+        {
+            get { return (LearnableModel)GetValue(SelectedLearnableProperty); }
+            set { SetValue(SelectedLearnableProperty, value); }
+        }
+        
+        public static readonly DependencyProperty SelectedLearnableProperty =
+            DependencyProperty.Register("SelectedLearnable", typeof(LearnableModel), typeof(CourseOverview), new PropertyMetadata(null));
+        
         #endregion
 
 
         #region API access
-        private void InitCourses()
+        private void InitCoursesList()
         {
-            SelectedCourse = null;
             List<CourseModel> currentCourses = new List<CourseModel>();
 
-            using (var client = new HttpClient())
+            using (var client = InitializeHttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:57696/");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
                 HttpResponseMessage response = client.GetAsync("api/course").Result;
-                if (response.IsSuccessStatusCode)
+                if (IsSuccessfullRequest(response, "Váratlan hiba történt, a kuzusok lekérdezése nem sikerült. Kérjük, próbálja később!"))
                 {
-                    //Getting the result and mapping to a Product object
-                    IEnumerable<CourseModel> courses = response.Content.ReadAsAsync<IEnumerable<CourseModel>>().Result;
-
-                    foreach (var course in courses)
-                    {
-                        currentCourses.Add(course);
-                    }
+                    currentCourses = response.Content.ReadAsAsync<IEnumerable<CourseModel>>().Result.ToList();
                 }
             }
 
+            int selectedCourseId = 0;
+            if (SelectedCourse != null)
+                selectedCourseId = SelectedCourse.CourseID;
+
             CourseList = currentCourses;
+            
+            if (selectedCourseId != 0)
+                SelectedCourse = CourseList.First(c => c.CourseID == selectedCourseId);
+        }
+
+        private void InitLearnablesList()
+        {
+            if (SelectedCourse == null)
+            {
+                Learnables = new List<LearnableModel>();
+                return;
+            }
+
+            List<LearnableModel> currentLearnables = new List<LearnableModel>();
+
+            using (var client = InitializeHttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync("api/learnable/" + SelectedCourse.CourseID).Result;
+                if (IsSuccessfullRequest(response, "Váratlan hiba történt, a szavak lekérdezése nem sikerült. Kérjük, próbálja később!"))
+                {
+                    currentLearnables = response.Content.ReadAsAsync<IEnumerable<LearnableModel>>().Result.ToList();
+                }
+            }
+
+            Learnables = currentLearnables;
+        }
+
+        private void SelectedCourse_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            InitLearnablesList();
         }
 
         private void AddCourse_Click(object sender, RoutedEventArgs e)
@@ -113,25 +155,13 @@ namespace LanguageLearner.WPF.Menu
                 return;
             }
 
-
-            using (var client = new HttpClient())
+            using (var client = InitializeHttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:57696/");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                CourseModel newCourse = new CourseModel();
-                newCourse.Name = NewCourseName;
-                var content = new StringContent(newCourse.ToString(), Encoding.UTF8, "application/json");
-
-                HttpResponseMessage response = client.PostAsJsonAsync("api/course", newCourse).Result;
-                if (response.IsSuccessStatusCode)
+                HttpResponseMessage response = client.PostAsJsonAsync("api/course", NewCourse).Result;
+                if (IsSuccessfullRequest(response, "Váratlan hiba történt, a kurzusfelvétel nem sikerült. Kérjük, próbálja később!"))
                 {
-                    InitCourses();
-                    NewCourseName = "";
-                }
-                else
-                {
-                    MessageBox.Show("Váratlan hiba történt, a kurzusfelvétel nem sikerült. Kérjük, próbálja később!", "Kurzusfelvétel hiba");
+                    InitCoursesList();
+                    NewCourse = new CourseModel();
                 }
             }
         }
@@ -144,32 +174,108 @@ namespace LanguageLearner.WPF.Menu
                 return;
             }
 
-            using (var client = new HttpClient())
+            using (var client = InitializeHttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:57696/");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
                 HttpResponseMessage response = client.DeleteAsync("api/course/" + SelectedCourse.CourseID).Result;
-                if (response.IsSuccessStatusCode)
+                if (IsSuccessfullRequest(response, "Váratlan hiba történt, a kurzus törlése nem sikerült. Kérjük, próbálja később!"))
                 {
-                    InitCourses();
-                }
-                else
-                {
-                    MessageBox.Show("Váratlan hiba történt, a kurzus törlése nem sikerült. Kérjük, próbálja később!", "Kurzustörlés hiba");
+                    SelectedCourse = null;
+                    InitCoursesList();
+                    InitLearnablesList();
                 }
             }
         }
 
+        private void NewLearnable_Click(object sender, RoutedEventArgs e)
+        {
+            LearnableEditor dialog = new LearnableEditor(CourseList, SelectedCourse);
+            if (dialog.ShowDialog().Value)
+            {
+                if (dialog.Word == null)
+                    return;
+
+                using (var client = InitializeHttpClient())
+                {
+                    HttpResponseMessage response = client.PostAsJsonAsync("api/learnable", dialog.Word).Result;
+                    if (IsSuccessfullRequest(response, "Váratlan hiba történt, a szó felvétele nem sikerült. Kérjük, próbálja később!"))
+                    {
+                        InitLearnablesList();
+                    }
+                }
+            }
+        }
+
+        private void EditLearnable_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedLearnable == null)
+            {
+                MessageBox.Show("Nincs kiválasztva a szó, amit szerkeszteni szeretnél!", "Szószerkesztés hiba");
+                return;
+            }
+
+            LearnableEditor dialog = new LearnableEditor(CourseList, SelectedLearnable);
+            if (dialog.ShowDialog().Value)
+            {
+                if (dialog.Word == null)
+                    return;
+
+                using (var client = InitializeHttpClient())
+                {
+                    HttpResponseMessage response = client.PutAsJsonAsync("api/learnable/" + dialog.Word.LearnableID, dialog.Word).Result;
+                    if (IsSuccessfullRequest(response, "Váratlan hiba történt, a szó módosítása nem sikerült. Kérjük, próbálja később!"))
+                    {
+                        InitLearnablesList();
+                    }
+                }
+            }
+        }
+
+        private void DeleteLearnable_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedLearnable == null)
+            {
+                MessageBox.Show("Nincs kiválasztva a szó, amit törölni szeretnél!", "Szótörlés hiba");
+                return;
+            }
+
+            using (var client = InitializeHttpClient())
+            {
+                HttpResponseMessage response = client.DeleteAsync("api/learnable/" + SelectedLearnable.LearnableID).Result;
+                if (IsSuccessfullRequest(response, "Váratlan hiba történt, a szó törlése nem sikerült. Kérjük, próbálja később!"))
+                {
+                    InitLearnablesList();
+                }
+            }
+        }
         #endregion
+
+        #region Helpers
+        private static HttpClient InitializeHttpClient()
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:57696/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        private static bool IsSuccessfullRequest(HttpResponseMessage response, String ErrorMessage)
+        {
+            bool success = response.IsSuccessStatusCode;
+            if (!success)
+                MessageBox.Show(ErrorMessage, "Probléma a szerkesztésben");
+
+            return success;
+        }
 
         private bool IsAvailableCourseName()
         {
-            return !String.IsNullOrEmpty(NewCourseName) &&
-                !CourseList.Select(c => c.Name).ToList().Contains(NewCourseName);
+            return !String.IsNullOrEmpty(NewCourse.Name) &&
+                !CourseList.Select(c => c.Name).ToList().Contains(NewCourse.Name);
         }
-
-
+        #endregion
+        
         #region Menu changes
         private void CoursesMenu_Click(object sender, RoutedEventArgs e)
         {
